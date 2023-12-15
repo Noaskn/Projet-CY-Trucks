0eb9db1022aae2fbbf641b142b88716756b45568
Noaskn <133594763+Noaskn@users.noreply.github.com>
2023-12-15 17:59:48 +0100

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -51,18 +51,30 @@ case $traitement in
 -h)
     cat help.txt
     ;;
-#Traitement qui donne les 10 conducteurs avec le plus de trajets par ordre décroissant
+#Traitement qui donne les 10 conducteurs avec le plus de trajets par ordre décroissant avec affichage du temps d'exécution
 -d1)
-   awk -F';' '{print $6}' $fichier_csv | sort | uniq -c | sort -rn | head -n 10 > top10_conducteurs_trajets.csv
-   ;;
-#Traitement qui donne les 10 conducteurs qui ont la plus longue distance par ordre décroissant
+    debut_temps=$(date +%s)
+    awk -F';' '!seen[$6,$1]++ {print $6}' $fichier_csv | sort | uniq -c | sort -k1,1nr | head -n 10 > top10_conducteurs_trajets.txt
+    fin_temps=$(date +%s)
+    duree=$((fin_temps - debut_temps))
+    echo "Temps d'exécution du traitement d1 : $duree secondes"
+    ;;
+#Traitement qui donne les 10 conducteurs qui ont la plus longue distance par ordre décroissant avec affichage du temps d'exécution
 -d2)
-   awk -F';' '{distances[$6]+=$5} END {for (i in distances) print distances[i], i}' $fichier_csv | sort -rn | head -n 10 > top10_conducteurs_distances.csv
-   ;;
+    debut_temps=$(date +%s)
+    awk -F';' '{distances[$6]+=$5} END {for (i in distances) print distances[i], i}' $fichier_csv | sort -rn | head -n 10 > top10_conducteurs_distances.txt
+    fin_temps=$(date +%s)
+    duree=$((fin_temps - debut_temps))
+    echo "Temps d'exécution du traitement d2 : $duree secondes"
+    ;;
+#Traitement qui donne les 10 numéros d'identifiant des trajets qui ont la plus longue distance par ordre croissant avec affichage du temps d'exécution
 -l)
-#Traitement qui donne les 10 numéros d'identifiant des trajets qui ont la plus longue distance par ordre croissant
-   awk -F';' '{distances[$1]+=$5} END {for (i in distances) print distances[i], i}' $fichier_csv | sort -k1,1nr | head -n 10 | sort -k2,2n > top10_trajets_distances.csv
-   ;;
+    debut_temps=$(date +%s)
+    awk -F';' '{distances[$1]+=$5} END {for (i in distances) print distances[i], i}' $fichier_csv | sort -k1,1nr | head -n 10 | sort -k2,2n > top10_trajets_distances.txt
+    fin_temps=$(date +%s)
+    duree=$((fin_temps - debut_temps))
+    echo "Temps d'exécution du traitement l : $duree secondes"
+    ;;
 -t)
     ;;
 -s)
