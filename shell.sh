5148518727dbd0941e86cdf992cead8e41721cdf
Noaskn <133594763+Noaskn@users.noreply.github.com>
2024-01-01 18:41:02 +0100

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -54,7 +54,7 @@ case $traitement in
 #Traitement qui donne les 10 conducteurs avec le plus de trajets par ordre décroissant avec affichage du temps d'exécution
 -d1)
     debut_temps=$(date +%s)
-    awk -F';' '!seen[$6,$1]++ {print $6}' $fichier_csv | sort | uniq -c | sort -k1,1nr | head -n 10 > "$temp/top10_conducteurs_trajets.txt"
+    awk -F';' '!seen[$6,$1]++ {print $6}' $fichier_csv | sort | uniq -c | sort -k1,1nr | head -n 10 > "$temp/traitement_d1.txt"
     fin_temps=$(date +%s)
     duree=$((fin_temps - debut_temps))
     echo "Temps d'exécution du traitement d1 : $duree secondes";;
@@ -62,7 +62,7 @@ case $traitement in
 #Traitement qui donne les 10 conducteurs qui ont la plus longue distance par ordre décroissant avec affichage du temps d'exécution
 -d2)
     debut_temps=$(date +%s)
-    awk -F';' '{distances[$6]+=$5} END {for (i in distances) print distances[i], i}' $fichier_csv | sort -rn | head -n 10 > "$temp/top10_conducteurs_distances.txt"
+    awk -F';' '{distances[$6]+=$5} END {for (i in distances) print distances[i], i}' $fichier_csv | sort -rn | head -n 10 > "$temp/traitement_d2.txt"
     fin_temps=$(date +%s)
     duree=$((fin_temps - debut_temps))
     echo "Temps d'exécution du traitement d2 : $duree secondes";;
@@ -70,16 +70,16 @@ case $traitement in
 #Traitement qui donne les 10 numéros d'identifiant des trajets qui ont la plus longue distance par ordre croissant avec affichage du temps d'exécution
 -l)
     debut_temps=$(date +%s)
-    awk -F';' '{distances[$1]+=$5} END {for (i in distances) print distances[i], i}' $fichier_csv | sort -k1,1nr | head -n 10 | sort -k2,2n > "$temp/top10_trajets_distances.txt"
+    awk -F';' '{distances[$1]+=$5} END {for (i in distances) print distances[i], i}' $fichier_csv | sort -k1,1nr | head -n 10 | sort -k2,2n > "$temp/traitement_l.txt"
     fin_temps=$(date +%s)
     duree=$((fin_temps - debut_temps))
     echo "Temps d'exécution du traitement l : $duree secondes";;
 
 -t)
-    ./projet "$fichier_csv" -t;;
+    ./projet "$fichier_csv" -t "$temp/traitement_t.txt";;
 
 -s)
-    ./projet "$fichier_csv" -s;;
+    ./projet "$fichier_csv" -s "$temp/traitement_s.txt";;
 
 *)
     echo "Le traitement saisi n'existe pas";;
