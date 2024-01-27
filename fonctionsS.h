93e4f8b308ed9971ceebd2ee51a4185d3176e793
Noaskn <133594763+Noaskn@users.noreply.github.com>
2024-01-27 17:27:27 +0100

    Update fonctionsS.h

--- fonctionsS.h
+++ fonctionsS.h
@@ -7,10 +7,10 @@
 
 AVL* creerAVL_s(int id_trajet, float distance);
 float moyenne(AVL* a);
-AVL* ajouterAVL_s(AVL* a, float distance, int id_trajet);
+AVL* ajouterAVL_s(AVL* a, float distance, int id_trajet, int* h);
 AVL* creerAVLtrier_s(float min, float max, float moy, int id_trajet);
-AVL* ajouterAVLtrier_s(AVL* a, float min, float max, float moy, int id_trajet);
-AVL* copier_trier_s(AVL* ancienAVL, AVL* nouveauAVL);
+AVL* ajouterAVLtrier_s(AVL* a, float min, float max, float moy, int id_trajet, int* h);
+AVL* copier_trier_s(AVL* ancienAVL, AVL** nouveauAVL, int* h);
 void stockageDonnees_s(AVL* a, char* mode, FILE* fichierSortie, int* compteur);
 
 #endif
